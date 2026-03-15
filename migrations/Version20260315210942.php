e2c1f6ef2e32a9e6ca9cea0701a23abd55bcd83c
Dimddimas <parasiteslixos@gmail.com>
2026-03-15 19:11:18 -0300

    neutraliza migration 20260315210942

--- migrations/Version20260315210942.php
+++ migrations/Version20260315210942.php
@@ -7,27 +7,18 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260315210942 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'neutralizada porque latitude e longitude já existem em market_units';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE market_units ADD latitude DOUBLE PRECISION DEFAULT NULL');
-        $this->addSql('ALTER TABLE market_units ADD longitude DOUBLE PRECISION DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE market_units DROP latitude');
-        $this->addSql('ALTER TABLE market_units DROP longitude');
     }
 }
