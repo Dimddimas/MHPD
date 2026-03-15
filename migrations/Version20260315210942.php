55d77677540f1e9ab7df397efbdb74ec1e7128be
Dimddimas <parasiteslixos@gmail.com>
2026-03-15 18:11:36 -0300

    add latitude longitude to market_units

---
+++ migrations/Version20260315210942.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260315210942 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE market_units ADD latitude DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE market_units ADD longitude DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE market_units DROP latitude');
+        $this->addSql('ALTER TABLE market_units DROP longitude');
+    }
+}
